a7768726fee083a15702f17b8d09d7c72cfe42f7
PRINCETON\Alejandro_Gudino <alejandro_gudino@epam.com>
2017-10-09 11:55:36 -0500

    My first commit

---
+++ HelloWorld.js
@@ -0,0 +1,41 @@
+var p = document.createElement('p');
+var hello = "Hello";
+var world = "World!";
+p.textContent = hello + " " + world;
+document.body.appendChild(p);
+function add(arg1) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    var total = arg1;
+    for (var i = 0; i < args.length; i++) {
+        total += args[i];
+    }
+    return total;
+}
+function substract(arg1) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    var total = arg1;
+    for (var i = 0; i < args.length; i++) {
+        total -= args[i];
+    }
+    return total;
+}
+function multiply(arg1) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    var total = arg1;
+    for (var i = 0; i < args.length; i++) {
+        total = total * args[i];
+    }
+    return total;
+}
+function divide(arg1, arg2) {
+    return arg1 / arg2;
+}
---
+++ HelloWorld.ts
@@ -0,0 +1,49 @@
+var p = document.createElement('p');
+var hello: string = "Hello";
+var world: string = "World!";
+p.textContent = hello + " " + world;
+
+document.body.appendChild(p);
+
+function add(arg1: number, ...args: number[]): number;
+function add(arg1: string, ...args: string[]): string;
+function add(arg1: any, ...args: any[]): any{
+  var total = arg1;
+  for(var i = 0; i < args.length; i++){
+    total += args[i];
+  }
+  return total;
+}
+
+function substract(arg1: number, ...args: number[]): number{
+  var total =  arg1;
+  for(var i = 0; i < args.length; i++) {
+    total -= args[i];
+  }
+  return total;
+}
+
+function multiply(arg1: number, ...args: number[]): number{
+  var total = arg1;
+  for(var i = 0; i < args.length; i++){
+    total = total * args[i];
+  }
+  return total;
+}
+
+function divide(arg1: number,  arg2: number): number{
+  return arg1 / arg2;
+}
+
+interface IAdder {
+  add(arg1: number, ...args: number[]): number;
+}
+
+interface ISubstractor {
+  substract(arg1: number, ...args: number[]): number;
+}
+
+interface ICalculator extends IAdder, ISubstractor {
+  multiply(arg1: number, ...args: number[]): number;
+  divide(arg1: number, arg2: number): number;
+}
